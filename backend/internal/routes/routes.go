8a66aa8c0f7b046f92162adfc9f04b70f9fbbf05
ivan-astan <astanin.i.a@gmail.com>
2025-01-20 20:22:36 +0300

    jwt functions

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -1,10 +1,13 @@
 package routes
 
 import (
+	"fmt"
 	"lifeSync/internal/models"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
@@ -14,9 +17,8 @@ func SetupRoutes(r *gin.Engine, db *gorm.DB) {
 	{
 		apiRoutes.POST("/register", RegisterUser(db))
 		apiRoutes.POST("/login", LoginUser(db))
-		apiRoutes.GET("/sadsd", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{"message": "saddsa"})
-		})
+		apiRoutes.GET("/createJWT", createToken(db))
+		apiRoutes.GET("/validateJWT", validateToken(db))
 	}
 
 	r.GET("/", func(c *gin.Context) {
@@ -74,5 +76,69 @@ func LoginUser(db *gorm.DB) gin.HandlerFunc {
 		// ...
 
 		c.JSON(http.StatusOK, gin.H{"message": "login successful"})
+	}
+}
+
+var mySigningKey = []byte("secret_key")
+
+func createToken(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var request struct {
+			Username string `json:"username"`
+		}
+
+		if err := c.ShouldBindJSON(&request); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		token := jwt.New(jwt.SigningMethodHS256)
+
+		claims := token.Claims.(jwt.MapClaims)
+		claims["username"] = request.Username
+		claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+		tokenString, err := token.SignedString(mySigningKey)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err := db.WithContext(c).Create(&models.Token{Token: tokenString, Userid: 2}).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "could not create token"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"token": tokenString})
+	}
+}
+
+func validateToken(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("invalid signing method")
+			}
+			return mySigningKey, nil
+		})
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			var storedToken models.Token
+			if err := db.Where("token = ?", tokenString).First(&storedToken).Error; err != nil {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+				return
+			}
+
+			c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Hello, %s!", claims["username"])})
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+		}
 	}
 }
