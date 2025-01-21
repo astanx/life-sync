7b2661476f38495fa307acf0747593e1d36d8b27
Ivan Astanin <ivanastanin@MacBook-Air.local>
2025-01-21 16:19:51 +0300

    database model changes

--- backend/internal/models/database.go
+++ backend/internal/models/database.go
@@ -9,3 +9,9 @@ type UserDB struct {
 	Username string `gorm:"type:text" json:"username"`
 	Password string `gorm:"type:text" json:"password"`
 }
+type TokenDB struct {
+	gorm.Model
+	Token  string `gorm:"unique"`
+	Userid uint   `gorm:"not null"`
+	User   User   `gorm:"foreignKey:Userid"`
+}
