3dfe01dc3844bf2bee3a15677b4d93399900431b
ivan-astan <astanin.i.a@gmail.com>
2025-01-26 12:37:17 +0300

    added tailwindcss

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,8 @@
 const App = () => {
   return (
-    <div>LifeSync</div>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+  </div>
   )
 }
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [react()],
+  },
+  plugins: [react(), tailwindcss()],
 });
