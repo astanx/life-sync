ba7b3112d391bc27cd77bc43dc56c38e22dbc542
ivan-astan <astanin.i.a@gmail.com>
2025-01-26 12:39:50 +0300

    main fix

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,9 +1,9 @@
 const App = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-    <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
-  </div>
-  )
-}
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
