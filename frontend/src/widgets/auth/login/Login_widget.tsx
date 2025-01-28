59962f82798359a3f247c68197672a2b0c99930c
Ivan <astanin.i.a@gmail.com>
2025-01-28 18:03:35 +0300

    added routing to login & register page

--- frontend/src/widgets/auth/login/Login_widget.tsx
+++ frontend/src/widgets/auth/login/Login_widget.tsx
@@ -1,8 +1,13 @@
 import { LoginForm } from "@/features/auth/login_form";
 import classes from "./Login_widget.module.css";
 import { Logo } from "@/shared/ui/logo";
+import { useNavigate } from "react-router-dom";
 
 const LoginWidget = () => {
+  const navigate = useNavigate();
+  const handleRegisterClick = () => {
+    navigate("/register");
+  };
   return (
     <div>
       <div className={classes.logo_container}>
@@ -16,7 +21,9 @@ const LoginWidget = () => {
         <LoginForm />
         <span>
           Don't have an account yet?{" "}
-          <span className={classes.register}>Register now</span>
+          <span className={classes.register} onClick={handleRegisterClick}>
+            Register now
+          </span>
         </span>
       </div>
     </div>
--- frontend/src/widgets/auth/register/Register_widget.tsx
+++ frontend/src/widgets/auth/register/Register_widget.tsx
@@ -1,8 +1,13 @@
 import { Logo } from "@/shared/ui/logo";
 import classes from "./Register_widget.module.css";
 import { RegisterForm } from "@/features/auth/register_form";
+import { useNavigate } from "react-router-dom";
 
 const RegisterWidget = () => {
+  const navigate = useNavigate();
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
   return (
     <div>
       <div className={classes.logo_container}>
@@ -16,7 +21,9 @@ const RegisterWidget = () => {
         <RegisterForm />
         <span>
           Already have an account?{" "}
-          <span className={classes.login}>Login now!</span>
+          <span className={classes.login} onClick={handleLoginClick}>
+            Login now!
+          </span>
         </span>
       </div>
     </div>
