b036a2d395c0be86a7a94f4f8d471d1071aefe1c
Ivan <astanin.i.a@gmail.com>
2025-02-05 16:10:57 +0300

    added small prop to logo

--- frontend/src/shared/ui/logo/Logo.tsx
+++ frontend/src/shared/ui/logo/Logo.tsx
@@ -1,8 +1,18 @@
 import company_logo from "@/shared/assets/images/company_logo.png";
 import classes from "./Logo.module.css";
+import { FC } from "react";
 
-const Logo = () => {
-  return <img src={company_logo} className={classes.logo} />;
+interface Props {
+  small?: boolean;
+}
+
+const Logo: FC<Props> = ({ small }) => {
+  return (
+    <img
+      src={company_logo}
+      className={`${classes.logo} ${small ? classes.small : ""}`}
+    />
+  );
 };
 
 export { Logo };
